efa8f95b00f67f630540730c00e5c8f1b4a7dbe9
hoshi <devnull@localhost>
2009-07-14 15:48:02 +0000

    Use gocept runner.

--- src/zeit/archive/worker.py
+++ src/zeit/archive/worker.py
@@ -1,6 +1,10 @@
+import sys
+import gocept.runner
 import zeit.archive.interfaces
+import zeit.archive.volume
 
 
+@gocept.runner.once()
 def build_main():
     url = sys.argv[1]
     zeit.archive.volume.rebuildVolume(url)
