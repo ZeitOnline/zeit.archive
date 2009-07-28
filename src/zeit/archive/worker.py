57a4c692070b3a9dfb65c4f46d54879216bbefde
hoshi <devnull@localhost>
2009-07-28 08:57:18 +0000

    Added principal.

--- src/zeit/archive/worker.py
+++ src/zeit/archive/worker.py
@@ -4,7 +4,7 @@ import zeit.archive.interfaces
 import zeit.archive.volume
 
 
-@gocept.runner.once()
+@gocept.runner.once(principal="zope.volumebuilder")
 def build_main():
     url = sys.argv[1]
     zeit.archive.volume.rebuildVolume(url)
