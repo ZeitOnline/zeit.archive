52132ec80157deacc62fcfe3adf1e9320a082de7
hoshi <devnull@localhost>
2009-09-16 09:35:23 +0000

    Merge from branch hoshi-publish-cps.

--- src/zeit/archive/tests.py
+++ src/zeit/archive/tests.py
@@ -9,12 +9,24 @@ ArchiveLayer = zope.app.testing.functional.ZCMLLayer(
     pkg_resources.resource_filename(__name__, 'ftesting.zcml'),
     __name__, 'ArchiveLayer', allow_teardown=True)
 
+
+# Use a rules file which contains only a syntax error so we don't have any
+# rules.
+cp_config = zeit.content.cp.testing.product_config['zeit.content.cp'].copy()
+cp_config['rules-url'] = 'file://%s' % pkg_resources.resource_filename(
+            'zeit.content.cp.tests.fixtures', 'syntax_error')
+
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
         layer=ArchiveLayer,
-        product_config=zeit.content.cp.testing.product_config,
-        ))
+        product_config={
+            'zeit.content.cp': cp_config,
+            'zeit.workflow': {'publish-script': 'cat',
+                              'path-prefix': ''}
+        }
+    ))
 
     return suite
