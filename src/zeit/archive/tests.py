d73ef9c0edc11820adc5f74ea926ab4ca4f6aa5a
Christian Zagrodnick <cz@gocept.com>
2010-08-05 16:26:57 +0000

    - Fix tests after product config changes in zeit.cms (re #7549).

--- src/zeit/archive/tests.py
+++ src/zeit/archive/tests.py
@@ -9,7 +9,9 @@ import zope.app.testing.functional
 
 ArchiveZCMLLayer = zeit.cms.testing.ZCMLLayer(
     'ftesting.zcml',
-    product_config=zeit.content.cp.testing.product_config)
+    product_config=(
+        zeit.cms.testing.cms_product_config +
+        zeit.content.cp.testing.product_config))
 
 
 class ArchiveLayer(ArchiveZCMLLayer):
