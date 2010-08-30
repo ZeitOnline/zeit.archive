462c0c31aa5571adb61a7cd00480999b1f1a754e
Christian Zagrodnick <cz@gocept.com>
2010-08-30 10:22:19 +0000

    back to development

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.archive',
-    version='0.9.0',
+    version='0.9.1dev',
     author='Dominik Hoppe',
     author_email='dominik.hoppe@zeit.de',
     url='http://trac.gocept.com/zeit',
