3d0c73814ce30048825b1a570e6cd66f3831b49a
Christian Zagrodnick <cz@gocept.com>
2012-12-17 11:41:52 +0100

    Preparing release 0.10.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.archive',
-    version='0.9.3dev',
+    version='0.10.0',
     author='Dominik Hoppe',
     author_email='dominik.hoppe@zeit.de',
     url='http://trac.gocept.com/zeit',
